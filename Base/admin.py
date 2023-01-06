7e29e311a877825e0cc7ecd0363b328e6700fdd2
Shagbaor Agber(Pyman) <67190277+Codewithshagbaor@users.noreply.github.com>
2023-01-06 04:11:47 +0100

    Delete Base directory

--- Base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserManager
-from Base.models import User, Verification, Post
-
-admin.site.register(User)
-admin.site.register(Verification)
-admin.site.register(Post)
-admin.site.unregister(Group)
